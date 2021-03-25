30ddbc1f1a111e209ecea76a17b7b5eee5d2acaa
Alex <alxshark@mail.ru>
2021-03-25 19:53:21 +0300

    показал задание на разбери

--- rpi/gpio/sample.py
+++ rpi/gpio/sample.py
@@ -3,12 +3,16 @@ import time
 
 N = 8 #количество пинов
 channel = range(N) #берём номер пина на плате из классического  (от 0 до 7) номера пина
-pins = [2, 3, 4, 17, 27, 22, 10, 9]
+#pins = [21,20,16,12,7,8,25,24]
+pins = [24, 25, 8, 7, 12, 16, 20, 21]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pins, GPIO.OUT)
 
 #GPIO.output(,)
+
+def darkness():
+    GPIO.output(pins, 0) #прямое действие
 
 def lightUpPro(ledNumber, period):
     n = pins[ledNumber]
@@ -59,13 +63,110 @@ def decToBinList(decNumber):
 
     return ans
 
+def  runningPatternContinue(pattern, direction, period):
+    d = decToBinList(pattern)
+    print(d)
+    for j in range(direction):
+        #lightNumberPro(pattern, period)
+        for i in range(8):
+            d = d[1:]+d[0:1]
+            print(d)
+            for s in range(N):
+                if d[s]==1:
+                    GPIO.output(pins[N-s-1], 1)
+                         #chis = pattern%(2**(N-i))*(2**i)+pattern//(2**(N-i))
+            time.sleep(period)
+            darkness()
+
+    #pattern1 = pattern
+    #for j in range(direction):
+     #   for i in range(8):
+     #       lightNumberPro(pattern, period)
+     #       pattern = pattern>>1
+     #   pattern = pattern1<<8
+      #  for i in range(8):
+      #      lightNumberPro(pattern, period)
+      #      pattern = pattern>>1
+    #lightNumberPro(pattern, period)
+
+#direction - смещаем в одну
 def  runningPattern(pattern, direction):
-    return pattern<<direction
+    if direction==1:
+        pattern = pattern>>1
+    else:
+        pattern = pattern
 
-lightUp(0, 2)
-blink(1, 5, 1)
-runningLight(2, 1)
-runningDark(2, 1)
+    lightNumber(pattern)
 
+def lightNumberPro(number, period):
+    d = decToBinList(number)
+    #print(d)
+    for i in range(N):
+        if d[i]==1:
+            GPIO.output(pins[N-i-1], 1)
+
+    time.sleep(period)
+    darkness()
+
+
+def lightNumber(number):
+    d = decToBinList(number)
+    print(d)
+    for i in range(N):
+        if d[i]==1:
+            GPIO.output(pins[N-i-1], 1)
+
+    time.sleep(2)
+    darkness()
+
+def shim(period):
+
+    #без библиотек - каждая итерация цикла - 0.01 секунды
+    #распределяем это время на вкл и выкл
+
+    i = 0
+    step = 1000/(period*100)
+    while i<1000:
+        GPIO.output(pins, 1) #прямое действие
+        time.sleep(i/100000)
+        GPIO.output(pins, 0) #прямое действие
+        time.sleep((1000-i)/100000)
+        i+=step
+
+    GPIO.output(pins, 1) #прямое действие
+    time.sleep(1)
+
+    i = 0
+    while i<1000:
+            GPIO.output(pins, 1) #прямое действие
+            time.sleep((1000-i)/100000)
+            GPIO.output(pins, 0) #прямое действие
+            time.sleep(i/100000)
+            i+=step
+
+
+    #for i in range(255):
+    #GPIO.output(pins, 1) #прямое действие
+    #time.sleep(2)
+    #GPIO.output(pins, 0) #прямое действие
+        #time.sleep(0.005)
+        #GPIO.output(pins, 0) #прямое действие
+        #time.sleep(0.005)
+
+
+#lightUp(4, 2)
+#blink(1, 5, 0.1)
+#runningDark(2, 0.1)
+#shim(10)
+m = 5 #число, которое я показываю
+runningPatternContinue(m, 2, 0.5)
+#runningPatternContinue(m, 0, 0.5)
+#print(p2)
+#lightNumber(m)
+#lightNumber(p1)
+#lightNumber(p2)
+#runningDark(10, 0.5)
+
+darkness()
 
 GPIO.cleanup()
